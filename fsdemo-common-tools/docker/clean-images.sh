50208c5f2467d254c3213dd48cecc79e5094a9c3
Bryne Lee <bryne_lxd@sina.com>
2020-03-09 01:02:45 +0800

    updated docker-compose.yml to add fsdemo-gateway to the docker-compose cluster.

--- fsdemo-common-tools/docker/clean-images.sh
+++ fsdemo-common-tools/docker/clean-images.sh
@@ -2,4 +2,7 @@ docker rmi xdorg1/fsdemo-frontend
 docker rmi xdorg1/fsdemo-usercenter
 docker rmi xdorg1/transactionagentservice
 docker rmi xdorg1/fsdemo-priceservice
+docker rmi xdorg1/fsdemo-gateway
+
+
 
