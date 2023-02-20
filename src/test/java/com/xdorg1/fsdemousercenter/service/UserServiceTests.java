57d6fe93559f794c9df76a3e93248a1d838c2247
Bryne Lee <bryne_lxd@sina.com>
2023-02-21 00:32:32 +0800

    added initdb.sh script for initializing postgresql database

--- src/test/java/com/xdorg1/fsdemousercenter/service/UserServiceTests.java
+++ src/test/java/com/xdorg1/fsdemousercenter/service/UserServiceTests.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class UserServiceTests {
+public class        UserServiceTests {
 
     @Autowired
     UserService userService;
